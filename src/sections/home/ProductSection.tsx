7ebbcc964b51696846e41c2954af946dc464b573
Raj Amancha <130662468+rajamancha@users.noreply.github.com>
2023-07-25 19:34:09 +0530

    Prdouct list ui fixes (#4)

--- src/sections/home/ProductSection.tsx
+++ src/sections/home/ProductSection.tsx
@@ -1,13 +1,14 @@
 import Section from "@/components/section";
 import { getAllProduct } from "@/services/product.services";
 import { ProductType } from "@/types/product.types";
-import { Stack } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import ProductCard from "../product-list/ProductCard";
 
 const ProductSection = () => {
-  const [productList, setProductList] = useState({});
+  const [productList, setProductList] = useState<ProductType[]>([]);
   useEffect(() => {
-    // getCalls();
+    getCalls();
   }, []);
 
   const getCalls = async () => {
@@ -41,6 +42,15 @@ const ProductSection = () => {
             totam ad provident.
           </p>
         </Stack>
+        <Grid container spacing={2}>
+          {productList &&
+            productList?.length &&
+            productList?.map((item) => (
+              <Grid item key={item.id} md={3} xs={6}>
+                <ProductCard productData={item} />
+              </Grid>
+            ))}
+        </Grid>
       </Section>
     </>
   );
--- src/sections/product-list/ProductListSlider.tsx
+++ src/sections/product-list/ProductListSlider.tsx
@@ -1,34 +1,32 @@
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import Image from "next/image";
-import {assets} from "@/assets";
+import { assets } from "@/assets";
 
 const ProductListSlider = () => {
   return (
     <div className="container">
       <Box
         sx={{
-          height: {xs: "calc(50vh - 60px)", md: "calc(50vh - 70px)"},
-          background: "lightBlue",
+          height: { xs: "calc(50vh - 60px)", md: "calc(70vh - 70px)" },
+          background: `url(${assets.images.HERO_IMAGE_JPG.src}) no-repeat center top/cover`,
+          backgroundAttachment: "fixed",
+          position: "relative",
+          zIndex: "1",
+          overflow: "hidden",
+          mb : 10,
+          "&:before": {
+            content: "''",
+            position: "absolute",
+            background: "rgba(0,0,0,0.2)",
+            zIndex: -1,
+            inset: "0px",
+            transition: "all 0.3s",
+          },
         }}
-      >
-        <Box className="container" height={"100%"}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-              gap: 2,
-              height: "100%",
-            }}
-          >
-            <Image src={assets.images.HERO_IMAGE} alt={'hero image'}/>
-          </Box>
-        </Box>
-      </Box>
+      ></Box>
     </div>
-  )
-}
+  );
+};
 
-export default ProductListSlider
+export default ProductListSlider;
