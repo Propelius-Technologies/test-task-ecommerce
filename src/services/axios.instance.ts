4fc640dde623f62d0a4e792eee058a096f81449e
Raj Amancha <raj.amancha@propelius.tech>
2023-07-25 17:23:14 +0530

    axios minor changes

--- src/services/axios.instance.ts
+++ src/services/axios.instance.ts
@@ -3,8 +3,6 @@ import { API_PATH } from "./axios.path";
 
 const axiosInstance = axios.create({
   baseURL: API_PATH,
-  timeout: 1000,
-  headers: { "X-Custom-Header": "*" },
 });
 
 export default axiosInstance;
