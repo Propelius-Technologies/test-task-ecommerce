9a0da9638274eae7217be2c3e4f2ceb91bc03dfd
Raj Amancha <130662468+rajamancha@users.noreply.github.com>
2023-07-25 19:36:23 +0530

    feat: cart sidebar UI (#5)
    Co-authored-by: Mayur Rana <mayur.rana@propelius.tech>

---
+++ src/components/side-bar/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {Box, Divider, Drawer} from "@mui/material";
+import {Close} from "@mui/icons-material";
+
+interface SidebarProps {
+  isOpen: boolean
+
+  close(): void
+}
+
+const Sidebar: React.FC<SidebarProps> = ({isOpen, close}) => {
+  return (
+    <Drawer
+      open={isOpen}
+      onClose={close}
+      anchor={"right"}
+    >
+      <Box
+        sx={{width: 400}}
+        role="presentation"
+        onClick={close}
+        onKeyDown={close}
+      >
+        <Box>
+          <Box sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: '20px'
+          }}>
+            <h3>Cart</h3>
+            <Close sx={{cursor: "pointer"}}/>
+          </Box>
+          <Divider/>
+        </Box>
+        <Box></Box>
+      </Box>
+    </Drawer>
+  )
+}
+
+export default Sidebar
