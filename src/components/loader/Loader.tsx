aef33e5e17728e033bdfe53a6c60e3177cf27033
Raj Amancha <raj.amancha@propelius.tech>
2023-07-26 16:45:05 +0530

    added README.md and loader changes

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -7,7 +7,7 @@ const Loader = () => {
     <Box
       sx={{
         height: { xs: "calc(100vh - 60px)", md: "calc(100vh - 70px)" },
-        width: "100vw",
+        width: "100%",
         zIndex: "2",
         display: "flex",
         alignItems: "center",
